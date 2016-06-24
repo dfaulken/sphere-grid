9857f338c820ff9d661c97dabde9cdb7d66e797c
David Faulkenberry <david.faulkenberry@gmail.com>
2016-06-23 22:02:48 -0400

    Function abstraction and more nodes

--- app/assets/javascripts/sphere_grid.js
+++ app/assets/javascripts/sphere_grid.js
@@ -36,27 +36,10 @@ $(document).ready(function(){
     _.each(node.connections, function(connection){
       drawConnection(node.x, node.y, connection[0], connection[1]);
     });
-    if(node.attribute_name){
-      circle.attr({fill: ATTRIBUTE_COLORS[node.attribute_name]});
-      CANVAS.text(node.x, node.y - 4, ATTRIBUTE_ABBREVIATIONS[node.attribute_name]).attr('font-size', 8);
-      var valueFontSize = node.attribute_name == 'HP' ? 10 : 12;
-      CANVAS.text(node.x, node.y + 5, node.value).attr('font-size', valueFontSize);
-    }
-    else if(node.ability){
-      circle.attr({fill: 'pink', title: node.ability.name});
-      if(_.contains(node.ability.name, ' ')){
-        var abilityWords = node.ability.name.split(' ');
-        CANVAS.text(node.x, node.y - 4, abilityWords[0]).attr('font-size', 6);
-        CANVAS.text(node.x, node.y + 4, abilityWords[1]).attr('font-size', 6);
-      }
-      else CANVAS.text(node.x, node.y, node.ability.name).attr('font-size', 8);
-    }
-    else if(node.lock_level){
-      circle.attr({fill: 'black'});
-      CANVAS.text(node.x, node.y - 5, 'LV.').attr({fill: 'red', 'font-size': 8});
-      CANVAS.text(node.x, node.y + 5, node.lock_level).attr({fill: 'red', 'font-size': 12});
-    }
-    else circle.attr({fill: 'grey'});
+    if(node.attribute_name) drawStatNode(circle, node);
+    else if(node.ability) drawAbilityNode(circle, node);
+    else if(node.lock_level) drawLockNode(circle, node);
+    else circle.attr({fill: 'grey'}); // empty node
   }
 
   function drawConnection(start_x, start_y, end_x, end_y){
@@ -64,4 +47,26 @@ $(document).ready(function(){
     CANVAS.path(pathString).attr('stroke-width', 2).toBack();
   }
 
+  function drawStatNode(circle, node){
+    circle.attr({fill: ATTRIBUTE_COLORS[node.attribute_name]});
+    CANVAS.text(node.x, node.y - 4, ATTRIBUTE_ABBREVIATIONS[node.attribute_name]).attr('font-size', 8);
+    var valueFontSize = node.attribute_name == 'HP' ? 10 : 12;
+    CANVAS.text(node.x, node.y + 5, node.value).attr('font-size', valueFontSize);
+  }
+
+  function drawAbilityNode(circle, node){
+    circle.attr({fill: 'pink', title: node.ability.name});
+    if(_.contains(node.ability.name, ' ')){
+      var abilityWords = node.ability.name.split(' ');
+      CANVAS.text(node.x, node.y - 4, abilityWords[0]).attr('font-size', 6);
+      CANVAS.text(node.x, node.y + 4, abilityWords[1]).attr('font-size', 6);
+    }
+    else CANVAS.text(node.x, node.y, node.ability.name).attr('font-size', 6);
+  }
+
+  function drawLockNode(circle, node){
+    circle.attr({fill: 'black'});
+    CANVAS.text(node.x, node.y - 5, 'LV.').attr({fill: 'red', 'font-size': 8});
+    CANVAS.text(node.x, node.y + 5, node.lock_level).attr({fill: 'red', 'font-size': 12});
+  }
 });
