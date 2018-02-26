6d6a5c8734ef855119ccf5b37668d9fa29be96a5
David Faulkenberry <david.faulkenberry@gmail.com>
2018-02-26 09:54:47 -0500

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,39 +10,76 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627182049) do
+ActiveRecord::Schema.define(version: 20180226144718) do
 
-  create_table "abilities", force: :cascade do |t|
-    t.string   "name",         limit: 255
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.string   "ability_type", limit: 255
-    t.integer  "number",       limit: 4
+  create_table "abilities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
+    t.integer "ability_type"
+    t.integer "sequence_number"
   end
 
-  create_table "characters", force: :cascade do |t|
-    t.string   "name",            limit: 255
-    t.string   "color",           limit: 255
-    t.string   "base_attributes", limit: 255
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+  create_table "activations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "character_id"
+    t.integer "node_id"
   end
 
-  create_table "characters_nodes", id: false, force: :cascade do |t|
-    t.integer "character_id", limit: 4, null: false
-    t.integer "node_id",      limit: 4, null: false
+  create_table "attributes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
+    t.string "abbreviation"
+    t.string "color"
+    t.integer "sequence_number"
+    t.text "permitted_values"
   end
 
-  create_table "nodes", force: :cascade do |t|
-    t.integer  "x",              limit: 4
-    t.integer  "y",              limit: 4
-    t.string   "connections",    limit: 255
-    t.string   "attribute_name", limit: 255
-    t.integer  "value",          limit: 4
-    t.integer  "ability_id",     limit: 4
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.integer  "lock_level",     limit: 4
+  create_table "character_attributes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "character_id"
+    t.integer "attribute_id"
+    t.integer "default_value"
+  end
+
+  create_table "characters", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
+    t.string "color"
+  end
+
+  create_table "default_character_abilities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "character_id"
+    t.integer "ability_id"
+  end
+
+  create_table "node_locations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "x"
+    t.integer "y"
+    t.integer "ability_id"
+    t.text "connections"
+    t.integer "lock_level"
+    t.integer "default_attribute_id"
+    t.integer "default_value"
+  end
+
+  create_table "nodes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "node_location_id"
+    t.integer "user_id"
+    t.integer "attribute_id"
+    t.integer "value"
+    t.boolean "locked"
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
